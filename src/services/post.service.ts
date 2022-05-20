7740aad178959209a2868f210b12f243eb71d4e1
maoaeri <ngoctranvan214@gmail.com>
2022-05-20 17:18:38 +0700

    11 files modified, 5 added

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -1,57 +1,51 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { Post } from "src/models/post.model";
-import { environment } from "src/environments/environment";
-import { ErrorHandlerService } from "./error-handler.service";
-import { catchError } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../../../models/post.model';
+import { environment } from '../environments/environment';
+import { ErrorHandlerService } from './error-handler.service';
+import { catchError } from 'rxjs/operators';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PostService {
-    constructor(
-        private http: HttpClient,
-        private errorHandler: ErrorHandlerService
-    ){}
+  constructor(
+    private http: HttpClient,
+    private errorHandler: ErrorHandlerService
+  ) {}
 
-    createPost(post: Post){
-        return this.http.post(`${environment.apiUrl}/posts`, JSON.stringify(post))
-        .pipe(
-            catchError(this.errorHandler.handleError('createPost'))
-        )
-    }
+  createPost(post: Post) {
+    return this.http
+      .post(`${environment.apiUrl}/posts`, JSON.stringify(post))
+      .pipe(catchError(this.errorHandler.handleError('createPost')));
+  }
 
-    updatePost(id: number, updateInfo: Post){
-        return this.http.put(`${environment.apiUrl}/posts/${id}`, JSON.stringify(updateInfo))
-        .pipe(
-            catchError(this.errorHandler.handleError('updatePost'))
-        )
-    }
+  updatePost(id: number, updateInfo: Post) {
+    return this.http
+      .put(`${environment.apiUrl}/posts/${id}`, JSON.stringify(updateInfo))
+      .pipe(catchError(this.errorHandler.handleError('updatePost')));
+  }
 
-    deletePost(id: number){
-        return this.http.delete(`${environment.apiUrl}/posts/${id}`)
-        .pipe(
-            catchError(this.errorHandler.handleError('deletePost'))
-        )
-    }
-    
-    deleteAllPosts(){
-        return this.http.delete(`${environment.apiUrl}/posts`)
-        .pipe(
-            catchError(this.errorHandler.handleError('deleteAllPosts'))
-        )        
-    }
+  deletePost(id: number) {
+    return this.http
+      .delete(`${environment.apiUrl}/posts/${id}`)
+      .pipe(catchError(this.errorHandler.handleError('deletePost')));
+  }
 
-    getPost(id: number){
-        return this.http.get<Post>(`${environment.apiUrl}/posts/${id}`)
-        .pipe(
-            catchError(this.errorHandler.handleError('getPost', null))
-        )
-    }
-    
-    getAllPost(id: number){
-        return this.http.get<Post[]>(`${environment.apiUrl}/posts`)
-        .pipe(
-            catchError(this.errorHandler.handleError('getAllPosts', []))
-        )
-    }
+  deleteAllPosts() {
+    return this.http
+      .delete(`${environment.apiUrl}/posts`)
+      .pipe(catchError(this.errorHandler.handleError('deleteAllPosts')));
+  }
+
+  getPost(id: number) {
+    return this.http
+      .get<Post>(`${environment.apiUrl}/posts/${id}`)
+      .pipe(catchError(this.errorHandler.handleError('getPost', null)));
+  }
+
+  getAllPost(id: number) {
+    return this.http
+      .get<Post[]>(`${environment.apiUrl}/posts`)
+      .pipe(catchError(this.errorHandler.handleError('getAllPosts', [])));
+  }
 }
